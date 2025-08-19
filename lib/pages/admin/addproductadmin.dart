323da00143c66bbb507e226d02924cf3d0cda070
El-Nagar <dev.adhamelnagar7@gmail.com>
2025-08-19 23:00:25 +0300

    v2

--- lib/pages/admin/addproductadmin.dart
+++ lib/pages/admin/addproductadmin.dart
@@ -14,6 +14,7 @@ class Addproductadmin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Product"),
